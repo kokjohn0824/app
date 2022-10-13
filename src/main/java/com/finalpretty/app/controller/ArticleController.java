7a941ef9c6b4b3edbb939d7bb186b3683781bbac
bbbblf2 <bbbblf2@gmail.com>
2022-10-13 15:56:16 +0800

    整理import

--- src/main/java/com/finalpretty/app/controller/ArticleController.java
+++ src/main/java/com/finalpretty/app/controller/ArticleController.java
@@ -4,15 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.finalpretty.app.model.Article;
 import com.finalpretty.app.repositories.ArticleRespository;

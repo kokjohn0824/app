cd3d5aa408f2b8c398e95b275595a855e0b1176b
Sharon <chuluvchu@gmail.com>
2022-10-13 11:08:43 +0800

    新增OderRespository+Oder_detailRespository+ProductRespository

---
+++ src/main/java/com/finalpretty/app/repositories/Oder_detailRespository.java
@@ -0,0 +1,8 @@
+package com.finalpretty.app.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.finalpretty.app.model.Order_detail;
+
+public interface Oder_detailRespository  extends JpaRepository< Order_detail, Long>{
+    
+}
---
+++ src/main/java/com/finalpretty/app/repositories/ProductRespository.java
@@ -0,0 +1,8 @@
+package com.finalpretty.app.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.finalpretty.app.model.Product;
+
+public interface ProductRespository  extends JpaRepository< Product, Long>{
+    
+}

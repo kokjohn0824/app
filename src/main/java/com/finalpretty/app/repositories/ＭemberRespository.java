c7145bde7c487875c463809c933d90074cb24c50
bbbblf2 <bbbblf2@gmail.com>
2022-10-13 10:26:15 +0800

    新增 member repostory

---
+++ src/main/java/com/finalpretty/app/repositories/ＭemberRespository.java
@@ -0,0 +1,8 @@
+package com.finalpretty.app.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.finalpretty.app.model.Member;
+
+public interface ＭemberRespository  extends JpaRepository< Member, Long>{
+    
+}

697500bd0f3f9f78a12b0fe567c57bac57863cc2
FranticSilence <ryan0818@gmail.com>
2022-10-13 11:27:59 +0800

    修改

--- src/main/java/com/finalpretty/app/repositories/ArticleRespository.java
+++ src/main/java/com/finalpretty/app/repositories/ArticleRespository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.finalpretty.app.model.Article;
 
 public interface ArticleRespository extends JpaRepository< Article, Long>{
-598529+8
+
 }

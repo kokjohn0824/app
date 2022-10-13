6c61277d0b630c7e4ceea0df0404cc5f488f33b3
bbbblf2 <bbbblf2@gmail.com>
2022-10-13 10:52:48 +0800

    deleted demo

--- src/main/java/com/finalpretty/app/repositories/ＭemberRespository.java
+++
@@ -1,8 +0,0 @@
-package com.finalpretty.app.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.finalpretty.app.model.Member;
-
-public interface ＭemberRespository  extends JpaRepository< Member, Long>{
-    
-}

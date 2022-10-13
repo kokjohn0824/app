9240bca8faa307db0c8ecf4a2a30c52674b9d999
bbbblf2 <bbbblf2@gmail.com>
2022-10-13 16:36:51 +0800

    organize imports

--- src/main/java/com/finalpretty/app/controller/VideoController.java
+++ src/main/java/com/finalpretty/app/controller/VideoController.java
@@ -1,17 +1,13 @@
 package com.finalpretty.app.controller;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.finalpretty.app.model.Article;
 import com.finalpretty.app.model.Video;
 import com.finalpretty.app.repositories.VideoRespository;
 

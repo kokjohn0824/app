54fce7b2a129ac15dc21200e750ea1a526ae9ce3
bbbblf2 <bbbblf2@gmail.com>
2022-10-13 23:49:51 +0800

    新增 thymleaf 模板套用 及一些語法範例

--- src/main/java/com/finalpretty/app/controller/hello.java
+++ src/main/java/com/finalpretty/app/controller/hello.java
@@ -1,7 +1,10 @@
 package com.finalpretty.app.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -11,6 +14,17 @@ public class hello {
     public String hello1() {
         return "hello";
     }
+    
+    @PostMapping("/hello")
+    public String hellosubmit(
+        @RequestParam(required = false) String firstName,
+        @RequestParam(required = false) String lastName,
+        Model m) {
+        String username = firstName+lastName;
+        m.addAttribute("username",username);    
+        return "hello";
+    }
+
 
     @GetMapping("/hihi")
     @ResponseBody

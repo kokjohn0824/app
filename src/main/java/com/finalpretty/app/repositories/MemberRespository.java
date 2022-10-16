23b00fbf1cfb174c9ef3c6028d4e374d8e8a58ce
Albert <ray09250@gmail.com>
2022-10-17 00:41:15 +0800

    新增後台member manage

--- src/main/java/com/finalpretty/app/repositories/MemberRespository.java
+++ src/main/java/com/finalpretty/app/repositories/MemberRespository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.finalpretty.app.model.Member;
 
-public interface MemberRespository extends JpaRepository<Member, Long> {
+public interface MemberRespository extends JpaRepository<Member, Integer> {
 
 }

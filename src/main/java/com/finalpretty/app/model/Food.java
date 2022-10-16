b8f96cdaabdebef548250849e8956ec8a9485d81
bbbblf2 <bbbblf2@gmail.com>
2022-10-16 19:46:25 +0800

    modified:   src/main/java/com/finalpretty/app/model/Food.java

--- src/main/java/com/finalpretty/app/model/Food.java
+++ src/main/java/com/finalpretty/app/model/Food.java
@@ -22,7 +22,7 @@ public class Food {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "id")//TODO:修改此id的名稱為food id 並檢查其相關關聯性衝突
+	@Column(name = "id")//FIXME:修改此id的名稱為food id 並檢查其相關關聯性衝突
 	private Integer id;
 	
 	@Column(name = "foodname")

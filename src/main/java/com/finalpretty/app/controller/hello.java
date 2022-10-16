600e89a48e6f0a78e7cdde014d48d5e085089eea
bbbblf2 <bbbblf2@gmail.com>
2022-10-16 15:47:45 +0800

    新增編輯器將編寫送出並展示

--- src/main/java/com/finalpretty/app/controller/hello.java
+++ src/main/java/com/finalpretty/app/controller/hello.java
@@ -1,5 +1,7 @@
 package com.finalpretty.app.controller;
 
+import javax.swing.text.AbstractDocument.Content;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,27 +11,34 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class hello {
-    
+
+    // 範例網站的視圖解析Controller
     @GetMapping("/hello")
     public String hello1() {
         return "hello";
     }
-    
+
     @GetMapping("/Manager")
     public String Manager1() {
         return "helloManager";
     }
-    
+
     @PostMapping("/hello")
     public String hellosubmit(
-        @RequestParam(required = false) String firstName,
-        @RequestParam(required = false) String lastName,
-        Model m) {
-        String username = firstName+lastName;
-        m.addAttribute("username",username);    
+            @RequestParam(required = false) String firstName,
+            @RequestParam(required = false) String lastName,
+            Model m) {
+        String username = firstName + lastName;
+        m.addAttribute("username", username);
         return "hello";
     }
 
+    // CKeditor編輯器的結果回傳視圖編輯器
+    @PostMapping("/editorResult")
+    public String ckeditorResult(@RequestParam String content, Model m) {
+        m.addAttribute("content",content);
+        return "editorResult";
+    }
 
     @GetMapping("/hihi")
     @ResponseBody
@@ -37,8 +46,4 @@ public class hello {
         return "hihi";
     }
 
-    @GetMapping("/test")
-    public String test() {
-        return "test";
-    }
 }

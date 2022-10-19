94d774402720d2830d1ab374d49f3dd4974b003c
FranticSilence <ryan0818@gmail.com>
2022-10-19 20:21:04 +0800

    實作新增文章+顯示文章調整

--- src/main/java/com/finalpretty/app/controller/ArticleController.java
+++ src/main/java/com/finalpretty/app/controller/ArticleController.java
@@ -1,15 +1,24 @@
 package com.finalpretty.app.controller;
 
+import java.io.IOException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.finalpretty.app.model.Article;
 import com.finalpretty.app.repositories.ArticleRespository;
@@ -20,60 +29,82 @@ public class ArticleController {
 	@Autowired
 	private ArticleRespository articleR;
 
-	//後台
-	//顯示全部文章
+	// 後台
+	// 顯示全部文章
 	@GetMapping("/article/manage")
 	public String manageArticlePage(Model m) {
 		List<Article> list = articleR.findAll();
 		m.addAttribute("list", list);
 		return "/article/backEndManageArticle";
 	}
+	
+	// 顯示全部文章的圖片
+	@GetMapping("showImage/{id}")
+	public ResponseEntity<byte[]> showImage1(@PathVariable Integer id) {
+		System.out.println();
+		Article article = articleR.findById(id).get();
+		byte[] photoFile = article.getPicture();
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.IMAGE_JPEG);
+		return new ResponseEntity<byte[]>(photoFile, headers, HttpStatus.OK);
+	}
 
-	//刪除文章(by id)
+	//前往新增文章
+	@GetMapping("/article/add")
+	public String addArticle() {
+		return "/article/backEndAddArticlePage";
+	}
+
+	// 刪除文章(by id)
 	@GetMapping("/article/delete")
 	public String deleteArticle(@RequestParam(name = "article_id") Integer article_id) {
 		articleR.deleteById(article_id);
 		return "redirect:/article/manage";
 	}
 
-	@GetMapping("/article/add")
-	public String addArticle(Model model) {
+	// 新增文章
+	@PostMapping("/article/add")
+	public String addProduct(
+			@RequestParam(name = "title") String title,
+			@RequestParam(name = "text") String text,
+			@RequestParam(name = "file") MultipartFile file) {
 
-		Article a1 = new Article();
+		Article article = new Article();
 
-		model.addAttribute("article", a1);
-
-		// Messages lastestMsg = mService.findLastest();
-		// model.addAttribute("lastestMsg", lastestMsg);
-
-		List<Article> allArticle = articleR.findAll();
-
-		model.addAttribute("allArticle", allArticle);
+		try {
+			article.setTitle(title);
+			article.setText(text);
+			article.setPicture(file.getBytes());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		articleR.save(article);
 
 		return "/article/backEndAddArticlePage";
+
 	}
 
-	//前台 
-
-	//顯示全部文章
+	// 前台
+	// 顯示全部文章
 	@GetMapping("/article/categories")
-    public String articleCategories(Model m) {
+	public String articleCategories(Model m) {
 		List<Article> list = articleR.findAll();
 		m.addAttribute("list", list);
-        return "/article/frontEndArticleCategories";
-    }
+		return "/article/frontEndArticleCategories";
+	}
 
+	// 顯示選取文章
 	@GetMapping("/article/show")
-	public String showArticle(@RequestParam(name = "article_id") Integer article_id,Model m) {
+	public String showArticle(@RequestParam(name = "article_id") Integer article_id, Model m) {
 		Optional<Article> optional = articleR.findById(article_id);
 		Article article = optional.get();
 		m.addAttribute("article", article);
 		return "/article/frontEndShowArticle";
 	}
 
+
+
 	// =============================================================================================
-
-
 
 	@PostMapping("/article/post")
 	public String postMsg(@ModelAttribute(name = "article") Article article, Model model) {
@@ -91,15 +122,14 @@ public class ArticleController {
 		return "addArticlePage";
 	}
 
-//	@GetMapping("/article/page")
-//	public String showMessages(@RequestParam(name = "p", defaultValue = "1") Integer pageNumber, Model model) {
-//		Page<Article> page = articleR.findByPage(pageNumber);
-//		model.addAttribute("page", page);
-//
-//		return "messages/showArticle";
-//	}
-
-	
+	// @GetMapping("/article/page")
+	// public String showMessages(@RequestParam(name = "p", defaultValue = "1")
+	// Integer pageNumber, Model model) {
+	// Page<Article> page = articleR.findByPage(pageNumber);
+	// model.addAttribute("page", page);
+	//
+	// return "messages/showArticle";
+	// }
 
 	@GetMapping("/article/edit")
 	public String editArticle(@RequestParam(name = "id") Integer id, Model model) {
@@ -116,25 +146,26 @@ public class ArticleController {
 		return "redirect:/article/page";
 	}
 
-//	@ResponseBody
-//	@PostMapping("/article/api/post")
-//	public List<Article> postMessagsApi(@RequestBody ArticleRespository articleR) {
-//		String userInput = articleR.getInputText();
-//
-//		// ...
-//
-//		Messages newMsg = new Messages();
-//		newMsg.setText(userInput);
-//
-//		articleR.insert(newMsg);
-//
-//		Page<Article> page = articleR.findByPage(1);
-//		return page.getContent();
-//	}
+	// @ResponseBody
+	// @PostMapping("/article/api/post")
+	// public List<Article> postMessagsApi(@RequestBody ArticleRespository articleR)
+	// {
+	// String userInput = articleR.getInputText();
+	//
+	// // ...
+	//
+	// Messages newMsg = new Messages();
+	// newMsg.setText(userInput);
+	//
+	// articleR.insert(newMsg);
+	//
+	// Page<Article> page = articleR.findByPage(1);
+	// return page.getContent();
+	// }
 
 	// @GetMapping("/article/ajax")
 	// public String getAjaxPage() {
-	// 	return "article/ajax-article";
+	// return "article/ajax-article";
 	// }
 
 }

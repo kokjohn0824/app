0ea9fe8245e14fcc51fa7a40d657889c3653d7b2
bbbblf2 <bbbblf2@gmail.com>
2022-10-19 23:57:50 +0800

    FIXME

--- src/main/java/com/finalpretty/app/controller/Appcontroller.java
+++ src/main/java/com/finalpretty/app/controller/Appcontroller.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+// FIXME: 將appcontroller改大寫
 @Controller
 @RequestMapping("/")
 public class Appcontroller {

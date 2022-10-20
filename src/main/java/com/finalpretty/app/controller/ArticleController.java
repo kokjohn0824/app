4b7fa341edc03b05ef816113fd977282ee9df5da
FranticSilence <ryan0818@gmail.com>
2022-10-20 15:55:18 +0800

    修改

--- src/main/java/com/finalpretty/app/controller/ArticleController.java
+++ src/main/java/com/finalpretty/app/controller/ArticleController.java
@@ -81,7 +81,7 @@ public class ArticleController {
 		return "/article/backEndAddArticlePage";
 	}
 
-	// // 修改文章
+	// // 修改文章sdadsad
 	// @GetMapping("/article/edit")
 	// public String editArticle(@RequestParam(name = "article_id") Integer id, Model model) {
 	// 	Optional<Article> a1 = articleR.findById(id);

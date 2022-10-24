ad6f93ec02a642151edec8cf25797b574c03c299
FranticSilence <ryan0818@gmail.com>
2022-10-24 17:42:54 +0800

    hjghj

---
+++ src/main/java/com/finalpretty/app/Response/ArticleResponse.java
@@ -0,0 +1,15 @@
+package com.finalpretty.app.Response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@NoArgsConstructor
+public class ArticleResponse {
+    private Integer member_id;
+    private Integer article_id;
+}

980bff820073de6875c172c0a21cf7ad61effe40
Sharon <chuluvchu@gmail.com>
2022-10-24 19:46:10 +0800

    商城.購物車.logo

--- src/main/java/com/finalpretty/app/product/controller/StoreFrontendController.java
+++ src/main/java/com/finalpretty/app/product/controller/StoreFrontendController.java
@@ -1,22 +1,42 @@
 package com.finalpretty.app.product.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.finalpretty.app.model.Product;
+import com.finalpretty.app.product.service.ProductService;
+import com.finalpretty.app.repositories.ProductRespository;
+
 @Controller
-@RequestMapping("/storefrontend")
+// @RequestMapping("/storefrontend")
 public class StoreFrontendController {
 
-    @GetMapping
+    @Autowired
+    private ProductService pService;
+
+    @GetMapping("/storefrontendone")
     public String addProduct() {
         return "product/storefrontend";
     }
 
-    
-    @GetMapping("/findAllProduct")
-    public String findAllproduct() {
-        return "product/productAll";
+    // 前台
+    // 顯示全部文章
+
+    @GetMapping("/storefrontend")
+    public String getAllProduct(Model model) {
+        List<Product> list = pService.findAll();
+
+        for (Product li : list) {
+            System.out.println(li.getPicture());
+        }
+        model.addAttribute("productlist", list);
+
+        return "/product/storefrontend";
     }
-    
+
 }

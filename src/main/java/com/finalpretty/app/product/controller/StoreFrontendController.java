335e29ba8b3ac51957233f01d9ab3a9b954f9693
Sharon <chuluvchu@gmail.com>
2022-10-24 12:04:22 +0800

    前台

--- src/main/java/com/finalpretty/app/product/controller/StoreFrontendController.java
+++ src/main/java/com/finalpretty/app/product/controller/StoreFrontendController.java
@@ -1,25 +1,8 @@
 package com.finalpretty.app.product.controller;
 
-import java.io.IOException;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
-
-import com.finalpretty.app.model.Product;
-import com.finalpretty.app.product.service.ProductService;
 
 @Controller
 @RequestMapping("/storefrontend")
@@ -30,4 +13,10 @@ public class StoreFrontendController {
         return "product/storefrontend";
     }
 
+    
+    @GetMapping("/findAllProduct")
+    public String findAllproduct() {
+        return "product/productAll";
+    }
+    
 }

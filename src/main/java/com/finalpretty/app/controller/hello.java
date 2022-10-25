c1acd6187625bb42f82e989c14d6cbc5eae80818
bbbblf2 <bbbblf2@gmail.com>
2022-10-25 19:20:56 +0800

    整理 import

--- src/main/java/com/finalpretty/app/controller/hello.java
+++ src/main/java/com/finalpretty/app/controller/hello.java
@@ -1,18 +1,15 @@
 package com.finalpretty.app.controller;
 
 import java.io.UnsupportedEncodingException;
-import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 

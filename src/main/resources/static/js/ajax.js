826820466c77fac53e5ccf88e18296fb8a5a85e5
bbbblf2 <bbbblf2@gmail.com>
2022-11-02 14:00:58 +0800

    更動ajax 架構

---
+++ src/main/resources/static/js/ajax.js
@@ -0,0 +1,31 @@
+const postdata = (url, requestJSON, callbackfunction) => {
+  let myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+  let registerdata = JSON.stringify(requestJSON);
+  let requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: registerdata,
+    redirect: "follow",
+  };
+  fetch(url, requestOptions)
+    .then((response) => response.json())
+    .then((result) => {
+      callbackfunction(result);
+    })
+    .catch((error) => console.error("error", error));
+};
+const getdata = (url, callbackfunction) => {
+  let requestOptions = {
+    method: "GET",
+    redirect: "follow",
+  };
+  fetch(url, requestOptions)
+    .then((response) => response.json())
+    .then((result) => {
+      tableData = result;
+      callbackfunction(result);
+    })
+    .catch((error) => console.error("error", error));
+};
+console.log("ajax!");
